0eafe074d65b517b2f1bcbacdd0820d82dbe89c5
Andy Yong <ayong@mdsol.com>
2014-02-11 15:07:42 -0500

    New rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DanceEvents::Application.config.secret_token = '0938a8db098fca0b021cb68af5ab3dd3778f9665975fc0aa9779e780b34c795c9861d24107f5472602fa0a5c844e6bd19b586071fb3eadecdc57c9bcff377d76'
