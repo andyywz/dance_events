016008bac5e5b642d3a54c59a9cd90b55ad8808a
Andy Yong <ayong@mdsol.com>
2014-02-11 16:36:26 -0500

    Adds backend and bootstrap

--- app/assets/javascripts/list.js
+++ app/assets/javascripts/list.js
@@ -1,8 +1,15 @@
-var ListCtrl = function ($scope) {
-  $scope.events = [{name: "Stompology"}, {name: "Babble"}];
+var app = angular.module("List", ["ngResource"]);
+
+app.factory("Event", ["$resource", function ($resource) {
+  return $resource("/events/:id.json", {id: "@id"}, {update: {method: "PUT"}});
+}]);
+
+app.controller("ListCtrl", ["$scope", "Event", function ($scope, Event) {
+  $scope.events = Event.query();
   
   $scope.addEvent = function () {
-    $scope.events.push($scope.newEvent);
+    var event = Event.save($scope.newEvent);
+    $scope.events.push(event);
     $scope.newEvent = {};
   };
-};
+}]);
