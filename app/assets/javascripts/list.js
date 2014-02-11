470c15d49d4eafc17f26671a1aa2d8b7b73a1963
Andy Yong <ayong@mdsol.com>
2014-02-11 15:32:53 -0500

    Adds addEvent functionality

--- app/assets/javascripts/list.js
+++ app/assets/javascripts/list.js
@@ -1,3 +1,8 @@
 var ListCtrl = function ($scope) {
-
-}
+  $scope.events = [{name: "Stompology"}, {name: "Babble"}];
+  
+  $scope.addEvent = function () {
+    $scope.events.push($scope.newEvent);
+    $scope.newEvent = {};
+  };
+};
