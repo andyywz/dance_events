c6967cca730e55a095ce18fcf6bef25ccac343fd
Andy Yong <ayong@mdsol.com>
2014-02-12 13:53:21 -0500

    Refactors into partials and adds edit and delete functionality

--- app/assets/javascripts/list.js
+++ app/assets/javascripts/list.js
@@ -1,4 +1,4 @@
-var app = angular.module("Event", ["ngResource", "Filters"]);
+var app = angular.module("eventApp", ["ngResource", "Filters"]);
 
 app.factory("Event", ["$resource", function ($resource) {
   return $resource("/events/:id.json", {id: "@id"}, {update: {method: "PUT"}});
@@ -17,5 +17,31 @@ app.controller("EventsCtrl", ["$scope", "Event", function ($scope, Event) {
     var event = Event.save($scope.newEvent);
     $scope.events.push(event);
     $scope.newEvent = {};
+  };
+  
+  $scope.updateEvent = function (ev) {
+    $scope.editObject = ev;
+  };
+  
+  $scope.saveEvent = function (ev) {
+    ev.$update();
+    $('#modal').modal('hide');
+  };
+  
+  $scope.deleteEvent = function (ev) {
+    Event.delete(ev);
+    $scope.events = updateEvents($scope.events, ev);
+  };
+  
+  var updateEvents = function (array, ev) {
+    var newArray = [];
+    
+    array.forEach(function (value) {
+      if (value !== ev) {
+        newArray.push(value)
+      };
+    });
+    
+    return newArray;
   };
 }]);
